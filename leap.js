5e91c877814922405a870b0efc2ee6997d1187f9
Yvan SRAKA <yvan.sraka@gmail.com>
2016-04-21 12:58:29 +0200

    Create leap.js

---
+++ leap.js
@@ -0,0 +1,27 @@
+'use strict';
+
+/**
+ * Represents a year to check whether is leap or not
+ *
+ * @param  {number} year
+ * Numeric year.
+ */
+function Year(year) {
+  this.year = year;
+}
+
+/**
+ * Whether given year is a leap year.
+ *
+ * @return {boolean}
+ * Whether given year is a leap year.
+ */
+Year.prototype.isLeap = function () {
+  //
+  // YOUR CODE GOES HERE
+  // (you have to use this.year)
+  //
+}
+
+module.exports = Year;
+
